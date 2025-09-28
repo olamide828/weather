9d338b8c57a1efc7d2e111aee74f62b5fd0ccc9b
olamide828 <adegboyega00001@gmail.com>
2025-09-28 05:04:53 -0700

    assignment

--- canvas.js
+++ canvas.js
@@ -8,7 +8,7 @@ const ctx = canvas.getContext("2d");
 
 // ctx.beginPath();
 // ctx.moveTo(20, 20);
-// ctx.lineTo(300, 20);
+// ctx.lineTo(250, 20);
 // ctx.stroke();
 
 // ctx.beginPath();
@@ -22,20 +22,97 @@ const ctx = canvas.getContext("2d");
 //     ctx.fillRect(event.clientX, event.clientY, 100, 100);
 // })
 
-let x = 0;
-let speed = 2
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(x, 50, 50, 50);
-    x += speed;
-    if (x + 50 >= canvas.width || x <= 0) {
-        speed = -speed;
-    }
+// let x = 0;
+// let speed = 2
+// function animate() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.fillRect(x, 50, 50, 50);
+//     x += speed;
+//     if (x + 50 >= canvas.width || x <= 0) {
+//         speed = -speed;
+//     }
 
-    requestAnimationFrame(animate);
-}
+//     requestAnimationFrame(animate);
+// }
 
-animate();
+// animate();
 
+
+
+
+// Draw and animate a bouncing ball
+// let y = 50;
+// let Speed = 3;
+// function animation() {
+//     ctx.beginPath();
+//     ctx.fillStyle = "silver"
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.arc(250, y, 50, 0, Math.PI * 2);
+//     ctx.fill();
+//     y += Speed;
+//     if (y + 50 > canvas.height || y - 50 < 0) {
+//         Speed = -Speed;
+//     }
+//     requestAnimationFrame(animation);
+// }
+
+// animation();
+
+
+ctx.beginPath();
+ctx.moveTo(100, 250);
+ctx.lineTo(250, 100);
+ctx.lineTo(400, 250);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.moveTo(100, 250);
+ctx.lineTo(400, 250);
+ctx.lineTo(400, 450);
+ctx.lineTo(100, 450);
+ctx.lineTo(100, 250);
+ctx.stroke();
+
+ctx.fillStyle = "#654321";
+ctx.fillRect(230, 320, 40, 130);
+
+ctx.fillStyle = "white"
+ctx.fillRect(110, 260, 70, 80);
+
+ctx.fillStyle = "white"
+ctx.fillRect(320, 260, 70, 80);
+
+// ctx.beginPath();
+// ctx.moveTo(200, 200);
+// ctx.lineTo(250, 10);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(400, 200);
+// ctx.lineTo(250, 10);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(200, 200);
+// ctx.lineTo(400, 200);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(200, 100);
+// ctx.lineTo(400, 200);
+// ctx.stroke();
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
